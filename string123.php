b23a45a7c64bc76b49b0d49540e9103d7727c9b3
Łukasz Wieczorek <LukaszWieczorek6618@gmail.com>
2025-09-11 09:23:50 +0200

    negr

---
+++ string123.php
@@ -0,0 +1,9 @@
+<?php
+//aby wypisac niektore znaki stosujemy tzw. znaki modyfikacji, ucieczki
+echo 'Arnold pedzioł kiesik "Jo wrócym"<br>';
+echo 'You deleted C:\\Windaws\\System32<br>';
+echo "To sie nie uozszerzy: \n nowa linijko \n";
+$Number=5;
+//zmienne w stringach zapisane w "" są interpretowane
+echo "Zmienna = $Number";
+?>
