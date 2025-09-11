eb695baa32d22390977af6af63ca6a2e10cc50d9
LWieczorek6618 <lukaszwieczorek6618@gmail.com>
2025-09-11 10:35:19 +0200

    zadanie

--- string123.php
+++ string123.php
@@ -15,6 +15,41 @@ END;
 echo <<<END
         a
        b
-      c
+      c <br>
       END;
+
+//Nowdoc generuje sformatowany tekst bez interpretacji znaków modyfikacji
+echo <<<'EOD'
+Jakisik tekst fajnuiśki tu se damy kropecke. ukośnicek jakisik /\ i bedzie elegancko.
+EOD;
+
+//strlen, mb_strlen zwraca długość stringów
+echo "<br>";
+echo strlen ("Hello World!"); //12
+echo "<br>";
+echo strlen ("Grzegżółka"); //13
+echo "<br>";
+echo mb_strlen ("Grzegżółka"); //10
+echo "<br>";
+//zamienie słowo na inne podane
+echo str_replace ("żyd", "murzyn", "tam jest żyd");
+echo "<br>";
+//wyszuka na ktorej pozycji zaczyna sie szukany tekst
+echo strpos("Witej negrze", 'negrze');
+$tekst = "Pochwalony";
+echo "<br>";
+printf("[%s]\n", $tekst);
+printf("[%10s]\n", $tekst);
+printf("[%-10s]\n", $tekst);
+printf("[%.4s]\n", $tekst);
+printf("[%10.3s]\n", $tekst);
+printf("[%-10.3s]\n", $tekst);
+$n=4395179;
+printf("%%d = '%d'\n", $n); //zwykłe
+printf("%%f = '%f'\n", $n); //floating
+printf("%%e = '%e'\n", $n); //naukowa notifikacja
+printf("%%b = '%b'\n", $n); //reprezentacja bitowa
+printf("%%o = '%o'\n", $n); //reprezentacja oktalna
+printf("%%s = '%s'\n", $n); //reprezentacja stringowa
+printf("%%x = '%x'\n", $n); //retrezentacja szesnastkowa
 ?>
