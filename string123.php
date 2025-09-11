6879270ea4e33c87962825897f38931292159709
LWieczorek6618 <lukaszwieczorek6618@gmail.com>
2025-09-11 09:29:31 +0200

    Hajtnler

--- string123.php
+++ string123.php
@@ -6,4 +6,15 @@ echo "To sie nie uozszerzy: \n nowa linijko \n";
 $Number=5;
 //zmienne w stringach zapisane w "" są interpretowane
 echo "Zmienna = $Number";
+echo <<<END
+        a
+      b
+    c
+\n
+END;
+echo <<<END
+        a
+       b
+      c
+      END;
 ?>
