c205edc3e44f8530ff0ec3c88af81041b4a6f7e4
LWieczorek6618 <lukaszwieczorek6618@gmail.com>
2025-09-19 14:08:54 +0200

    oper

--- operatory.php
+++ operatory.php
@@ -1,10 +1,12 @@
 <?php
-$a=3;
-$b=&$a;
-print “$a\n”;
-print “$b\n”;
-$a=4;
-print “$a\n”;
-print “$b\n”;
+$a = 3;
+$b = &$a;
 
+print "$a\n";
+print "$b\n";
+
+$a = 4;
+
+print "$a\n";
+print "$b\n";
 ?>
