aea7e46712c81304b05d18e1488f31dd93782149
LWieczorek6618 <lukaszwieczorek6618@gmail.com>
2025-09-19 14:09:03 +0200

    oper

--- operatory.php
+++ operatory.php
@@ -9,4 +9,13 @@ $a = 4;
 
 print "$a\n";
 print "$b\n";
+
+echo "<h3>Post increment</h3>";
+$c=5;
+echo "Powinno byc 5 " . $c++ . "<br/>\n";
+echo "Powinno byc 6 " . $c . "<br/>\n";
+echo "<h3>Pre increment</h3>";
+$c=5;
+echo "Powinno byc 6 " . ++$c . "<br/>\n";
+echo "Powinno byc 6 " . $c . "<br/>\n";
 ?>
